659b5fb36ce8f9e4ab9629c29460cbb7f21606ce
Jessica Ma <jma@jma01.phage.bcgsc.ca>
2018-01-15 17:11:48 -0800

    setup

--- src/btl/jessicama/AMP.java
+++ src/btl/jessicama/AMP.java
@@ -1,9 +1,19 @@
 package btl.jessicama;
 
-import uk.ac.ebi.kraken.annotations.*;
-
+import java.util.ArrayList;
+import java.io.File.*;
 
 public class AMP {
 
+    String id;
+    ArrayList<AMPComp> comparisonList;
 
+    AMP(String id) {
+        this.id = id;
+        comparisonList = new ArrayList<AMPComp>();
+    }
+
+    public static void main(String[] args) {
+    }
 }
+
